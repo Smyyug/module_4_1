f8c6ac6bc1ee06c055447179d8fd1cc676ab8795
Smyyug <100402danil@gmail.com>
2024-11-03 19:20:59 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from true_math import divide as high
+from fake_math import divide as school
+result1 = high(69, 3)
+result2 = high(3, 0)
+result3 = school(49, 7)
+result4 = school(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
